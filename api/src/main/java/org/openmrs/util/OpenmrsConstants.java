c65539bb775446fb1594197d7c5bf82295d7459c
tanmayg <tanmay.goel@nuagenetworks.net>
2017-11-12 20:11:31 -0500

    adding import for Random Class

--- api/src/main/java/org/openmrs/util/OpenmrsConstants.java
+++ api/src/main/java/org/openmrs/util/OpenmrsConstants.java
@@ -17,6 +17,7 @@ import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 import java.util.Properties;
+import java.util.Random;
 import java.util.Vector;
 
 import org.apache.commons.io.IOUtils;
